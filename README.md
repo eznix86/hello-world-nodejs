ef32f8aa0637895e7433b93d90f3c7f9ece2b9d3
Bernard Joseph Jean Bruno <brunobernard1996@outlook.com>
2018-04-04 20:09:26 +0400

    add html page

---
+++ README.md
@@ -0,0 +1,1 @@
+https://codeburst.io/node-js-by-example-part-1-668376cd4f96
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 "use strict";
 const http = require('http');
-
-const hostname = '127.0.0.1';
+const fs = require('fs');
+const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) =>{
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+fs.readFile('/opt/lampp/htdocs/index.html', (err, html)=>{
+    const server = http.createServer((req, res) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(html);
+    res.end();
 
-server.listen(port, hostname, ()=>{
-  console.log(`Server is running at http://${hostname}:${port}/`)
-})
+  }).listen(port, hostname, ()=>{
+    console.log(`Server is running at http://${hostname}:${port}/`)
+  });
+
+});
